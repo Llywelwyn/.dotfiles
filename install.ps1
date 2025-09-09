142d3bf7b6d4118f1834c241b8e8dd9430f7b2fa
Lewis Wynne <lewiiswynne@gmail.com>
2025-09-09 22:40:58 +0100

    installation with scoop && copying over profile

---
+++ install.ps1
@@ -0,0 +1,54 @@
+Write-Host "setting up scoop!"
+
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+  Write-Host "~ getting scoop!"
+  Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+  iwr -useb get.scoop.sh | iex
+} else {
+  Write-Host "~ found scoop, updating!"
+  scoop update
+}
+
+$buckets = scoop bucket list | ForEach-Object { $_.Name }
+
+Get-Content "./scoop/buckets.txt" | ForEach-Object {
+  $bucket = $_.Trim()
+  if ($bucket -and -not ($buckets -contains $bucket)) {
+    Write-Host "~ adding bucket: $bucket"
+    scoop bucket add $bucket
+  } else {
+    Write-Host "~ found bucket: $bucket"
+  }
+}
+
+$raw = & { scoop list *>&1 }
+$apps = $raw | Where-Object { $_ -is [pscustomobject] } | ForEach-Object { $_.Name }
+
+Get-Content "./scoop/apps.txt" | ForEach-Object {
+  $app = $_.Trim()
+  if ($app -and -not ($apps -contains $app)) {
+    Write-Host "~ installing app: $app"
+    scoop install $app
+  } else {
+    Write-Host "~ found app: $app"
+  }
+}
+
+Write-Host "~ all finished with scoop"
+
+Write-Host "~ copying powershell profile!"
+Copy-Item ./powershell/profile.ps1 $PROFILE -Force
+
+if ($MyInvocation.InvocationName -eq '.') {
+  . $PROFILE
+  Write-Host "~ reloaded profile"
+} else {
+  Write-Host "~ u didn't run this dot-sourced"
+  Write-Host "~ that's okay"
+  Write-Host "~ reload manually"
+  Write-Host "             with this"
+  Write-Host "                    command:"
+  Write-Host "                            . `$PROFILE"
+}
+
+. $PROFILE
---
+++ powershell/profile.ps1
@@ -0,0 +1,39 @@
+${function:~} = { Set-Location ~ }
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
+
+${function:dt} = { Set-Location ~\Desktop }
+${function:docs} = { Set-Location ~\Documents }
+${function:dl} = { Set-Location ~\Downloads }
+${function:nvimc} = { Set-Location $env:LOCALAPPDATA\nvim }
+
+Set-Alias time Measure-Command
+Set-Alias vi nvim
+Set-Alias vim nvim
+
+if (Get-Command wget.exe -ErrorAction SilentlyContinue | Test-Path) {
+  rm alias:wget -ErrorAction SilentlyContinue
+}
+
+if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:ls -ErrorAction SilentlyContinue
+    ${function:ls} = { ls.exe --color @args }
+    ${function:l} = { ls -lF @args }
+    ${function:la} = { ls -laF @args }
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+} else {
+    ${function:la} = { ls -Force @args }
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+}
+
+if (Get-Command curl.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:curl -ErrorAction SilentlyContinue
+    ${function:curl} = { curl.exe @args }
+    ${function:gurl} = { curl --compressed @args }
+} else {
+    ${function:gurl} = { curl -TransferEncoding GZip }
+}
+
