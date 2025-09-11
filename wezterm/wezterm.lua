4047fe8e033725d204349a5ed2cd350cbb9a994a
lew <lew@ily2.dev>
2025-09-11 13:04:11 +0100

    feat(wezterm): minor styling changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,14 +9,18 @@ config.font_size = 12.0
 config.font = wezterm.font("FiraCode Nerd Font", {})
 config.line_height = 1.2
 
-config.window_decorations = "RESIZE"
-config.initial_rows = 25
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.integrated_title_button_style = "Windows"
+config.integrated_title_buttons = { "Hide", "Close" }
+config.integrated_title_button_alignment = "Right"
+config.integrated_title_button_color = "auto"
+config.initial_rows = 30
 config.initial_cols = 80
 
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = false
 config.show_close_tab_button_in_tabs = false
-config.show_new_tab_button_in_tab_bar = false
-
+config.show_new_tab_button_in_tab_bar = true
 config.use_fancy_tab_bar = true
 config.tab_max_width = 25
 
@@ -39,10 +43,10 @@ end
 
 config.colors = theme.colors()
 config.window_frame = theme.window_frame()
-config.window_frame.font_size = 8
-config.window_background_opacity = 0.8
+config.window_frame.font_size = 10
+config.window_background_opacity = 1.0
 config.text_background_opacity = 0.4
-config.win32_system_backdrop = "Acrylic"
+config.win32_system_backdrop = "Auto"
 
 config.inactive_pane_hsb = { hue = 1.0, saturation = 0.7, brightness = 0.7 }
 
