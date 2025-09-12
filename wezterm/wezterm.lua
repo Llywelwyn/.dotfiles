b3c3042d00151397cf165c645330148a68258274
lew <lew@ily2.dev>
2025-09-12 14:30:32 +0100

    feat(wezterm): bg opacity/extremely minor gradient

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -44,9 +44,22 @@ end
 config.colors = theme.colors()
 config.window_frame = theme.window_frame()
 config.window_frame.font_size = 10
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.9
 config.text_background_opacity = 0.4
 config.win32_system_backdrop = "Auto"
+config.window_background_gradient = {
+	colors = {
+		"#241515",
+		"#331717",
+	},
+	orientation = {
+		Radial = {
+			cx = 0.75,
+			cy = 0.65,
+			radius = 1.0,
+		},
+	},
+}
 
 config.inactive_pane_hsb = { hue = 1.0, saturation = 0.7, brightness = 0.7 }
 
