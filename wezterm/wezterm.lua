687454f7441c98c12824d74d223ffbc41911f39a
lew <lew@ily2.dev>
2025-09-24 12:46:04 +0100

    feat(wezterm): theme tweak to better match nvim

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -35,17 +35,54 @@ config.disable_default_key_bindings = true
 config.keys = require("keys")
 
 local theme = {}
-if os.getenv("THEME") == "light" then
-	theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").dawn
-else
-	theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
-end
+theme.palette = {
+	fujiWhite = "#8F8536", -- default
+	oldWhite = "#C8C093", -- statuslines
+	sumiInk4 = "#54546D", -- line numbers, fold column, non-text characters, float borders
+	fujiGray = "#8F3F41", -- comments
+	oniViolet = "#FFFF00", -- statements, keywords
+	crystalBlue = "#A60000", -- functions and titles
+	waveAqua2 = "#00CECE", -- types
+	springViolet1 = "#FFFFFF", -- light foreground
+	springViolet2 = "#FFFF00", -- brackets and punctuation
+	springGreen = "#00BD00", -- strings
+	boatYellow2 = "#FFFF00", -- operators
+	sakuraPink = "#FFFFFF", -- numbers
+	carpYellow = "#8F8536", -- identifiers
+	surimiOrange = "#00CECE", -- constants, imports, booleans
+	springBlue = "#A60000", -- specials and builtin functions
+	peachRed = "#FFFF00", -- specials 2: exception handling, return
+}
 
-config.colors = theme.colors()
-config.window_frame = theme.window_frame()
-config.window_frame.font_size = 10
+config.colors = {
+	foreground = theme.palette.oldWhite,
+	cursor_bg = theme.palette.oldWhite,
+	cursor_border = theme.palette.oldWhite,
+	selection_fg = theme.palette.oldWhite,
+	ansi = {
+		theme.palette.oldWhite,
+		theme.palette.springBlue,
+		theme.palette.surimiOrange,
+		theme.palette.fujiWhite,
+		theme.palette.surimiOrange, -- Cmdline
+		theme.palette.peachRed, -- Donate button
+		theme.palette.waveAqua2,
+		theme.palette.fujiWhite,
+	},
+	brights = {
+		theme.palette.oldWhite,
+		theme.palette.crystalBlue, -- branches in git log?
+		theme.palette.springGreen, -- strings
+		theme.palette.crystalBlue, -- command
+		theme.palette.surimiOrange, -- directories
+		theme.palette.boatYellow2, -- current directory
+		theme.palette.surimiOrange, -- branch name
+		theme.palette.boatYellow2,
+	},
+}
+
 config.window_background_opacity = 0.95
-config.text_background_opacity = 0.15
+config.text_background_opacity = 0.20
 config.win32_system_backdrop = "Auto"
 config.window_background_gradient = {
 	colors = {
